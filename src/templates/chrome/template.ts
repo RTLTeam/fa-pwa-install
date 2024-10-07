48ebf3ff53e8a67d892c73e9c537da186c49bab0
Hamidreza Karimi <hkarimi561@gmail.com>
2024-10-07 21:38:56 +0330

    first commit

---
+++ src/templates/chrome/template.ts
@@ -0,0 +1,46 @@
+import { html } from 'lit';
+import { classMap } from 'lit/directives/class-map.js';
+import { WebAppManifest } from 'web-app-manifest';
+
+const template = (name: string, description: string, installDescription: string, disableDescription: boolean, disableScreenshots: boolean, disableClose: boolean, icon: string, manifest: WebAppManifest, installAvailable: any, hideDialog: any, install: any, toggleGallery: any, galleryRequested: boolean) => {
+    const installDialogClasses = () => { return {available: installAvailable, gallery: galleryRequested }};
+    const screenshotsAvailable = !disableScreenshots && manifest.screenshots && manifest.screenshots.length;
+
+    return html`
+        <div id="pwa-install-element">
+            <div class="install-dialog chrome ${classMap(installDialogClasses())}">
+                <div class="dialog-body">
+                    <div class="icon">
+                        <img src="${icon}" alt="icon" class="icon-image" draggable="false">
+                    </div>
+                    <div class="about">
+                        <div class="name">
+                            <label>${name}</label>
+                            ${!disableClose? html`<button class="material-button secondary close" @click='${hideDialog}'>
+                                <svg viewBox="0 0 24 24"><path d="M5.3 18.7c.2.2.4.3.7.3s.5-.1.7-.3l5.3-5.3 5.3 5.3a1.08 1.08 0 0 0 .7.3 1.08 1.08 0 0 0 .7-.3c.4-.4.4-1 0-1.4L13.4 12l5.3-5.3c.4-.4.4-1 0-1.4s-1-.4-1.4 0L12 10.6 6.7 5.3c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l5.3 5.3-5.3 5.3c-.4.4-.4 1 0 1.4z"/></svg>
+                            </button>` : ''}
+                        </div>
+                        <div class="hostname">${location.hostname}</div>
+                        
+                    </div>
+                    ${description ? html `<div class="description app-description">${description}</div>`: ''}
+                    ${!disableDescription? 
+                        html`<hr><div class="description install-description">${installDescription? installDescription: `${'این سایت دارای قابلیت اپلیکیشن می باشد.'} ${'برای تجربه گسترده و دسترسی آسان، آن را روی دستگاه خود نصب کنید.'}`}</div>` 
+                        : ''}
+                    ${screenshotsAvailable? html`<pwa-gallery .screenshots=${manifest.screenshots}></pwa-gallery>`: ''}
+                    <div class="action-buttons">
+                        ${screenshotsAvailable? html`<button class="material-button secondary" @click='${toggleGallery}'>${galleryRequested?'کمتر':'بیشتر'}</button>`:''}
+                        <button class="material-button primary install" @click='${install}'>نصب</button>
+                    </div>
+                </div>
+            </div>
+            <div class="install-dialog chrome mobile ${classMap(installDialogClasses())}">
+                <div class="dialog-body">
+                    <pwa-bottom-sheet .props=${{name, icon, description}} .disableClose=${disableClose} .install=${install} .hideDialog=${hideDialog}>
+                    </pwa-bottom-sheet>
+                    ${screenshotsAvailable? html`<pwa-gallery .screenshots=${manifest.screenshots}></pwa-gallery>`: ''}
+                </div>
+            </div>
+        </div>`;
+};
+export default template;
